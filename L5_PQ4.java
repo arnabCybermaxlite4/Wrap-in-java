0f7ae92347e19d451526e8e76beeb65150690a59
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-05-29 12:58:34 +0530

    Add files via upload

---
+++ L5_PQ4.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class L5_PQ4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number you want to check");
+        System.out.println("The number is " + sc.hasNextFloat ());
+    }
+}
