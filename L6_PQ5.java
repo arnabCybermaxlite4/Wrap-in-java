c012f03b19e3ea66c30866afbed903e74e71df55
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-05-31 21:49:23 +0530

    Add files via upload

---
+++ L6_PQ5.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class L6_PQ5 {
+    public static void main(String[] args) {
+        System.out.println("The result of the expression is :");
+        float a = (7/4.0f)*9/2.0f;
+        System.out.println(a);
+    }
+}
