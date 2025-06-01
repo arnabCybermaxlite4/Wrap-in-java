90e4cd18385cc2825dda2d8b4c73208d41e6f8c1
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-06-01 14:02:17 +0530

    Add files via upload

---
+++ L6_PQ7.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class L6_PQ7 {
+    public static void main(String[] args) {
+        int v = 10 ;
+        int u = 7 ;
+        int a = 2 ;
+        int s = 5 ;
+        int result = (v*v-u*u)/(2*a*s);
+        System.out.println("The result is : " + result);
+    }
+}
