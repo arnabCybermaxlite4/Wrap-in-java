9a4aa580aab3a0b1d8d09f36a217bd92da07115d
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-06-02 13:22:56 +0530

    Add files via upload

---
+++ L8_PQ8.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class L8_PQ8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Enter the String: ");
+        String str = sc.nextLine();
+        System.out.println("The length of the string is : " + str.length());
+        System.out.println(" The lowercase of the string is : " + str.toLowerCase());
+        System.out.println(" The uppercase of the string is : " + str.toUpperCase());
+    }
+}
