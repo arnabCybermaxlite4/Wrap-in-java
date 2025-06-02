108a2948b297d8333473339c6d1a1779a2b32b29
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-06-02 13:22:32 +0530

    Add files via upload

---
+++ L7_String.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class L7_String {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       /* String str = "Arnab";
+        System.out.println(str);
+        String str1 = new String ("Arnab");
+        System.out.println(str1);
+        int a =10;
+        System.out.printf("The value of a is : %d " ,a);
+        float b =20.5f;
+        System.out.printf("The value of b is : %f " ,b);
+        System.out.format("The value of a is : %d and The value of b is : %f",a,b);
+        */
+         // String st = sc.next();
+         // String st1= sc.nextLine();
+         //  System.out.println(st1);
+         //System.out.println(st);
+        int number1 =10;
+        System.out.printf("The value of number1 is : %d " , number1);
+        System.out.format("The value of number1 is : %d",number1);
+        float number2 =20.5f;
+        System.out.printf("The value of number2 is : %.2f " , number2);
+        System.out.format("The value of number2 is : %f",number2);
+    }
+}
