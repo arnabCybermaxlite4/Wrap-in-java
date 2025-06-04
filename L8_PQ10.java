0ba6ee6b6ac8dcc6447360f444f37dabaaf645fb
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-06-04 14:17:57 +0530

    Add files via upload

---
+++ L8_PQ10.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class L8_PQ10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the name: ");
+        String new_name = sc.nextLine();
+        System.out.println("Dear " + new_name + " Thank you for visiting our website") ;
+    }
+}
