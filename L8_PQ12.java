34c0c593a596ff12710f89795dbe53857377bb47
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-06-08 21:43:41 +0530

    Add files via upload

---
+++ L8_PQ12.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+
+public class L8_PQ12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String str = sc.nextLine();
+        System.out.println("The final Result is : ");
+        System.out.format("\" %s \" ",str);
+    }
+}
