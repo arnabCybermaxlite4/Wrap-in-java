0a62df20e8c335096de58991f6e09a41ebcb1275
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-06-09 11:48:04 +0530

    Add files via upload

---
+++ L9_conditional.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class L9_conditional {
+    public static void main(String[] args) {
+        int age = 28;
+        if(age>=18){
+            System.out.println("You can vote");
+        }
+        else{
+            System.out.println("You can't vote");
+        }
+    }
+}
