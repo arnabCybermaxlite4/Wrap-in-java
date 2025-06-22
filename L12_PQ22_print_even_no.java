8dbb922e12967a05ca73363c665d8e488f0321cd
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-06-22 21:42:55 +0530

    Add files via upload

---
+++ L12_PQ22_print_even_no.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class L12_PQ22_print_even_no {
+    public static void main(String[] args) {
+        int i =0;
+        System.out.println("The even numbers are : ");
+        do {
+            System.out.println("The number is: " +i);
+            i=i+2;
+        }
+        while(i<=100);
+    }
+}
