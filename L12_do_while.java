af85c511316e6ae70b18f08473781de8320dea3a
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-06-22 21:42:39 +0530

    Add files via upload

---
+++ L12_do_while.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class L12_do_while {
+    public static void main(String[] args) {
+        int i = 1;
+        do{
+            System.out.println("The value of i is : " + i);
+            i++;
+        }
+        while(i<=10);
+    }
+}
