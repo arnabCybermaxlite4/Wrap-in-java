9012ce7ae4eafc588212433ed549cd08d9822781
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-06-27 22:20:10 +0530

    Add files via upload

---
+++ L16_PQ30_pattern1.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class L16_PQ30_pattern1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println(" Enter the value of n: ");
+        int n = sc.nextInt();
+        for(int i =n ; i > 0 ; i-- ){
+            for (int j = 0;j<i;j++ ){
+                System.out.print(" * ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
