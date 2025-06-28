22f955220460417681d77bce42be6b5f644de159
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-06-28 13:18:34 +0530

    Add files via upload

---
+++ L16_PQ33_hollow_rectangle.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class L16_PQ33_hollow_rectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the row value n: ");
+        int n = sc.nextInt();
+        System.out.println(" Enter the column value m: ");
+        int m = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 || i == n || j == 1 || j == m) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
