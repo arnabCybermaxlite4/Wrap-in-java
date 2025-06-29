5f7de814c2f9688f9a0926fb559260e31d9b20c2
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-06-29 12:32:11 +0530

    Add files via upload

---
+++ L16_PQ34_Inverted_pyramid_no.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class L16_PQ34_Inverted_pyramid_no {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Enter the number of rows: ");
+        int rows = sc.nextInt();
+        for (int i = rows; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
