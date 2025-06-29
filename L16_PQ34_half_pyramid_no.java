9378dc1ad56b54c15152f839e0cc98304e440471
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-06-29 12:31:55 +0530

    Add files via upload

---
+++ L16_PQ34_half_pyramid_no.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class L16_PQ34_half_pyramid_no {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
