cf7a18b01e5f6f0aec7c8bf89f13ee6960461b91
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-06-30 12:51:08 +0530

    Add files via upload

---
+++ L16_PQ36_half_pyramid_with_numbers.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class L16_PQ36_half_pyramid_with_numbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n-i+1; j++) {
+                System.out.print(j);
+            }
+            System.out.println(" ");
+        }
+    }
+}
