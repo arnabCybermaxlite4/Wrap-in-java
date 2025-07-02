5b0c937fb85b2d2f1f7a4a765d4d4ccc8a654374
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-07-02 12:44:48 +0530

    Add files via upload

---
+++ L16_PQ40_butterfly_pattern.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class L16_PQ40_butterfly_pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int n = sc.nextInt();
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            // Left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // Spaces
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            // Right stars7
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n; i >= 1; i--) {
+            // Left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // Spaces
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+            // Right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
