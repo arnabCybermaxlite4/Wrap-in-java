dfcb1c1f5f89a26d69e2b7ea57fa3e7d50aa05df
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-07-06 12:23:21 +0530

    Add files via upload

---
+++ L16_PQ45_repeat_1.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class L16_PQ45_repeat_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println("the number is: " + i);
+        }
+    }
+}
