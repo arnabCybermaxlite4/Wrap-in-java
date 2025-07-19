dc84a7d649e6c3b84efa572962af312ecb488a92
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-07-19 14:35:27 +0530

    Add files via upload

---
+++ L20_PQ62_spiral_order_matrix.java
@@ -0,0 +1,59 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class L20_PQ62_spiral_order_matrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        System.out.println("Enter the number of columns: ");
+        int cols = sc.nextInt();
+        int[][] matrix = new int[rows][cols];
+        // taking input for the matrix
+        System.out.println("Enter the values for the matrix: ");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("The matrix is: ");
+        // printing the matrix
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println(); // new line after each row
+        }
+        // spiral order traversal
+        int top = 0, bottom = rows - 1, left = 0, right = cols - 1;
+        System.out.println("Spiral order of the matrix is: ");
+        while (top <= bottom && left <= right) {
+            // Traverse from left to right
+            for (int i = left; i <= right; i++) {
+                System.out.print(matrix[top][i] + " ");
+            }
+            top++;
+            // Traverse from top to bottom
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(matrix[i][right] + " ");
+            }
+            right--;
+            if (top <= bottom) {
+                // Traverse from right to left
+                for (int i = right; i >= left; i--) {
+                    System.out.print(matrix[bottom][i] + " ");
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                // Traverse from bottom to top
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(matrix[i][left] + " ");
+                }
+                left++;
+            }
+        }
+        System.out.println(); // for better output formatting
+    }
+}
