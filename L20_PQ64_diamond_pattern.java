559051f8ed095580b52a13d2d68d96c9ddc9fde7
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-07-20 23:17:45 +0530

    Add files via upload

---
+++ L20_PQ64_diamond_pattern.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class L20_PQ64_diamond_pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows for the diamond pattern:");
+        int n = sc.nextInt(); // number of rows for the upper half
+        // Upper half of the diamond
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half of the diamond
+        for (int i = n - 1; i >= 1; i--) {
+            // Print leading spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
