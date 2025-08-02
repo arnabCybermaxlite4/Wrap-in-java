bf4d0bea42fbf4b0ab7792bf91fe1a140997eb6e
Arnab Ganguly <arnabganguly1528@gmail.com>
2025-08-02 13:49:49 +0530

    Add files via upload

---
+++ L21_methods_PQ71_multiplication.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class L21_methods_PQ71_multiplication {
+    public static int multiply(int a , int b){
+        return a*b;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number: ");
+        int b = sc.nextInt();
+        int result = multiply(a, b);
+        System.out.println("The multiplication of " + a + " and " + b + " is: " + result);
+    }
+}
